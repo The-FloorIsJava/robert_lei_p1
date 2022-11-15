4d143b43ad9050073b2bad259da350b751b8da99
rlei <rlei004@odu.edu>
2022-11-15 12:55:09 -0500

    changed tables to reflect project requirements and optional requirements

--- Project1AccountsTickets.sql
+++ Project1AccountsTickets.sql
@@ -1,20 +1,24 @@
 DROP TABLE IF EXISTS ACCOUNTS CASCADE;
 CREATE TABLE ACCOUNTS (
-	userID serial PRIMARY KEY,
+	username varchar(30) PRIMARY KEY,
+	passcode varchar(50) NOT NULL--password reserved
 	firstName varchar(50),
 	lastName varchar(50),
+	address varchar(200),
+	profilePicture blob,
 	accountType varchar(20) DEFAULT 'employee',
-	username varchar(20),
-	passcode varchar(50) --password reserved
 );
 DROP TABLE IF EXISTS TICKETS CASCADE;
 CREATE TABLE TICKETS (
 	ticketID serial PRIMARY KEY,
 	ticketName varchar(40),
 	ticketType varchar(40) DEFAULT 'General',
-	ticketDescription varchar(10000),
-	submitterID int,
-	FOREIGN KEY(submitterID) REFERENCES ACCOUNTS(userID)
+	ticketDescription varchar(10000) NOT NULL,
+	amount numeric NOT NULL,
+	status varchar(50) DEFAULT 'Pending',
+	supportingImage blob,
+	submitterUser varchar(30),
+	FOREIGN KEY(submitterUser) REFERENCES ACCOUNTS(username)
 	ON DELETE CASCADE
 );
 
@@ -25,6 +29,6 @@ INSERT INTO ACCOUNTS(firstName, lastName, username, passcode) VALUES ('this', 'a
 SELECT * FROM ACCOUNTS;
 DELETE FROM ACCOUNTS WHERE lastName = 'also';
 SELECT * FROM ACCOUNTS;
-INSERT INTO TICKETS(ticketName, submitterid) SELECT 'send help!', userid FROM ACCOUNTS WHERE lastName = 'is'; 
+INSERT INTO TICKETS(ticketDescription, amount, submitterid) SELECT 'send help!', 0, username FROM ACCOUNTS WHERE lastName = 'is'; 
 SELECT * FROM TICKETS;
 */
