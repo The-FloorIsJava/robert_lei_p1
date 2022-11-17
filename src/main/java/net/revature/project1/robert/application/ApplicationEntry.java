db05ecf85322140f2b9ab2b9fcedaa7bb26e305f
rlei <rlei004@odu.edu>
2022-11-17 15:46:33 -0500

    finished connections

--- src/main/java/net/revature/project1/robert/application/ApplicationEntry.java
+++ src/main/java/net/revature/project1/robert/application/ApplicationEntry.java
@@ -1,8 +1,7 @@
 package net.revature.project1.robert.application;
 
 import io.javalin.Javalin;
-
-
+import net.revature.project1.robert.connections.ConnectionFactory;
 public class ApplicationEntry{
     public static void main(String[] args){
         Javalin app = Javalin.create().start(8008);
--- src/main/java/net/revature/project1/robert/connections/ConnectionFactory.java
+++ src/main/java/net/revature/project1/robert/connections/ConnectionFactory.java
@@ -1,13 +1,27 @@
 package net.revature.project1.robert.connections;
 
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.Properties;
 
 public class ConnectionFactory {
     private static final ConnectionFactory connectionFactory = new ConnectionFactory();
-
+    private Properties properties = new Properties();
     private ConnectionFactory(){
-
+        try{
+            properties.load(new FileReader("src/main/java/resources/db.properties"));
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+    static{
+        try{
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }
     }
     public ConnectionFactory getConnectionFactory(){
         return connectionFactory;
